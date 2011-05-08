915e2c3f38696ee68088c8a43a59bbc9d2ba330e
Piotr Sarnacki <drogus@gmail.com>
2011-05-08 12:38:00 +0200

    Fix specs

--- spec/bulk_api_spec.rb
+++ spec/bulk_api_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe "Bulk API" do
   it "should set app/bulk path" do
-    Rails.application.paths["app/bulk"].first.should == Rails.root.join("app/bulk")
+    Rails.application.paths["app/bulk"].first.should == Rails.root.join("app/bulk").to_s
   end
 
   it "should set app/sproutcore path" do
-    Rails.application.paths["app/sproutcore"].first.should == Rails.root.join("app/sproutcore")
+    Rails.application.paths["app/sproutcore"].first.should == Rails.root.join("app/sproutcore").to_s
   end
 end
