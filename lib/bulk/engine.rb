2967c76cf714c2ef738bf53d1bcca65e929c8668
Piotr Sarnacki <drogus@gmail.com>
2011-05-08 14:51:54 +0200

    Properly load sproutcore

--- lib/bulk/engine.rb
+++ lib/bulk/engine.rb
@@ -7,6 +7,11 @@ module Bulk
       ActiveRecord::Base.include_root_in_json = false
     end
 
+    initializer "require sproutcore Rack app" do
+      # it needs to be done after rails load, otherwise haml freaks out
+      require 'bulk/sproutcore'
+    end
+
     config.paths.add "app/bulk", :eager_load => true
     config.paths.add "app/sproutcore"
   end
--- lib/bulk_api.rb
+++ lib/bulk_api.rb
@@ -2,5 +2,4 @@ module Bulk
   require 'bulk/collection'
   require 'bulk/resource'
   require 'bulk/engine'
-  require 'bulk/sproutcore'
 end
