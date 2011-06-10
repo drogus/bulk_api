45816bcea9bfc94f3ecc97a6f2b7a1ee642409a8
Alexander Flatter <flatter@gmail.com>
2011-06-10 15:26:03 +0200

    Do not wrap parameters (Rails 3.1)

--- app/controllers/bulk/api_controller.rb
+++ app/controllers/bulk/api_controller.rb
@@ -1,4 +1,8 @@
 class Bulk::ApiController < ActionController::Base
+  # Do not wrap parameters into a nested hash. This behaviour was
+  # introduced with rails 3.1 and blows up resource handling.
+  wrap_parameters :format => [] if respond_to?(:wrap_parameters)
+
   def get
     options = Bulk::Resource.get(self)
     yield options if block_given?
