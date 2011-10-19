24227b7ddcb51ce77fa16996da741d78b4a27ce5
stevenbristol <stevenbristol@gmail.com>
2011-10-19 11:24:49 -0400

    CORS headers after_filter

--- app/controllers/bulk/api_controller.rb
+++ app/controllers/bulk/api_controller.rb
@@ -1,4 +1,5 @@
 class Bulk::ApiController < ActionController::Base
+  after_filter :set_cors_headers
   def get
     options = Bulk::Resource.get(self)
     yield options if block_given?
@@ -24,10 +25,15 @@ class Bulk::ApiController < ActionController::Base
   end
   
   def options
+    set_cors_headers
+    render :text => '', :content_type => 'text/plain'
+  end
+  
+  protected
+  def set_cors_headers
     headers['Access-Control'] = "allow *"
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
     headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, X-SproutCore-Version, Content-Type'
-    render :text => '', :content_type => 'text/plain'
   end
 end
